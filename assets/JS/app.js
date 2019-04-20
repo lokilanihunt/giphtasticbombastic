be7bef5f7a0ed27b174adb429e2ca61be837a8d0
Lokilani Hunt <lokilani@berkeley.edu>
2019-04-20 09:24:02 -0700

    giphy new new

---
+++ assets/JS/app.js
@@ -0,0 +1,57 @@
+$(document).ready()
+
+// Function that makes a button:
+//first create a listener that is looking at the submit buton next to the search bar
+$(".submitBtn").on("click", function(){
+
+   //create a button with class btn-info 
+   var newButton = $("<button>");
+   newButton.addClass("btn").addClass("btn-info");
+   newButton.text($(".form-control").val());
+
+   //set that buttons value to the text in the search bar
+   
+
+   //append that button
+   $(".buttonGroup").append(newButton);
+
+})
+
+
+// fucntion that takes whatever th etext is of my button and searches jquery for it
+$(document).on("click", ".btn-info", function(){
+    console.log("clicked");
+    searchGiphy($(this).text());
+
+})
+//Giphy Function--argument is the title of the button
+    var searchGiphy= function(search){
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=q3Jf5W8DlmFwyTKVeLoy1bQh7GH7tp0D&q="+search+"&limit=10&offset=0"
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method:"GET"
+        })
+        .then(function(response){
+            console.log(response);
+            console.log(response.data[0].images.original.url);
+            
+            //create variable to hold the gif url
+            var gifURL= response.data[0].images.original.url;
+            
+            //make a new div with the class of card
+            var newCard= $("<div>").addClass("card");
+            
+            //make a new link that leads to the gif
+            var newGif= $("<img>").attr("src", gifURL);
+            newCard.append(newGif)
+            
+            //append our gif into the card
+            $("#gifSection").prepend(newCard);
+            // append our dynamically created card with a gif to our gif section
+
+    // image_url
+    // title
+
+        })
+    }
